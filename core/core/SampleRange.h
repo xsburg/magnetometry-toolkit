1b4bc8925453da5071cda6043c7edd807c2f4da0
Stepan Burguchev <me@sburg.net>
2015-08-14 02:40:52 +0300

    write, validation

--- core/core/SampleRange.h
+++ core/core/SampleRange.h
@@ -16,6 +16,7 @@
 
 #include "Common/SmartPtr.h"
 #include "IBinaryStream.h"
+#include <Common/InvalidOperationException.h>
 
 namespace Greis
 {
@@ -35,16 +36,44 @@ namespace Greis
         virtual MSeedSampleType getSampleType() const = 0;
 
         inline QString channelName() const { return _channelName; }
-        inline void channelName(const QString& channelName) { _channelName = channelName; }
+        inline void channelName(const QString& channelName)
+        {
+            if (channelName.size() > 10)
+            {
+                throw Common::InvalidOperationException("Channel name should not be longer than 10 characters.");
+            }
+            _channelName = channelName;
+        }
 
         inline QString network() const { return _network; }
-        inline void network(const QString& network) { _network = network; }
+        inline void network(const QString& network)
+        {
+            if (network.size() > 10)
+            {
+                throw Common::InvalidOperationException("Network name should not be longer than 10 characters.");
+            }
+            _network = network;
+        }
 
         inline QString station() const { return _station; }
-        inline void station(const QString& station) { _station = station; }
+        inline void station(const QString& station)
+        {
+            if (station.size() > 10)
+            {
+                throw Common::InvalidOperationException("Station name should not be longer than 10 characters.");
+            }
+            _station = station;
+        }
 
         inline QString location() const { return _location; }
-        inline void location(const QString& location) { _location = location; }
+        inline void location(const QString& location)
+        {
+            if (location.size() > 10)
+            {
+                throw Common::InvalidOperationException("Location name should not be longer than 10 characters.");
+            }
+            _location = location;
+        }
 
         inline QDateTime startTime() const { return _startTime; }
         inline void startTime(const QDateTime& startTime) { _startTime = startTime; }
@@ -84,14 +113,55 @@ namespace Greis
 
         MSeedWriter(IBinaryStream::SharedPtr_t binaryStream) : _binaryStream(binaryStream)
         {
+            _recLen = 512;
+            _encoding = DE_STEIM2;
         }
 
-        void write(IntegerSampleRange::SharedPtr_t sampleRange)
-        {
-
-        }
+        void write(IntegerSampleRange::SharedPtr_t sampleRange);
 
     private:
         IBinaryStream::SharedPtr_t _binaryStream;
     };
+
+#include <libmseed.h>
+
+    inline void MSeedWriter::write(IntegerSampleRange::SharedPtr_t sampleRange)
+    {
+        // наполнения хедера mseed
+        MSRecord* msr = msr_init(NULL);
+
+        // общие для записей данные
+        strcpy(msr->network, sampleRange->network().toLatin1().constData());
+        strcpy(msr->station, sampleRange->station().toLatin1().constData());
+        strcpy(msr->location, sampleRange->location().toLatin1().constData());
+
+        msr->samprate = 1000.0 / header.samplingRate(); // число семплов в секунду
+
+        msr->reclen = _recLen;
+        msr->record = NULL;
+        msr->encoding = _encoding;  // compression
+        msr->byteorder = 1;         // big endian byte order
+
+        msr->sampletype = 'i';      // declare type to be 32-bit integers
+        //#endif
+        //#ifdef MSR_PACK_FUNC
+        strcpy(msr->channel, _chNames[channel - 1].c_str());
+
+        msr->starttime = starttime; // для каждого канала начальное время одно и тоже
+        msr->datasamples = &channelData[0]; // pointer to 32-bit integer data samples
+        msr->numsamples = channelData.size();
+
+        int psamples = 0;
+        int precords = msr_pack(msr, &recorder, _curFile, &psamples, 1, verbose);
+        if (precords == -1)
+        {
+            ms_log(2, "ERROR!!! Cannot pack records!\n");
+            exit(1);
+        }
+
+        msr->datasamples = 0;
+        msr_free(&msr);
+
+        ms_log(0, "Packed %d samples into %d records\n", psamples, precords);
+    }
 }
