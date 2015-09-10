e0d62ee197a39e8a3f5cdae78697110af8e6fee7
Stepan Burguchev <me@sburg.net>
2015-09-10 03:38:45 +0300

    manager

---
+++ core/EbDeviceManager.h
@@ -0,0 +1,26 @@
+﻿// ***********************************************************************
+// <author>Stephan Burguchev</author>
+// <copyright company="Stephan Burguchev">
+//   Copyright (c) Stephan Burguchev 2012-2015. All rights reserved.
+// </copyright>
+// <summary>
+//   EbDeviceManager.h
+// </summary>
+// ***********************************************************************
+#pragma once
+
+#include "common/SmartPtr.h"
+
+namespace core
+{
+    class EbDeviceManager
+    {
+    public:
+        SMART_PTR_T(EbDeviceManager);
+
+        void connect()
+        {
+            
+        }
+    };
+}
