116630e31cb7d670d076ff0874b2280493a8fea1
Stepan Burguchev <me@sburg.net>
2015-09-27 04:21:58 +0300

    request

--- web/routes/api/dashboard.js
+++ web/routes/api/dashboard.js
@@ -5,10 +5,23 @@
  *       All Rights Reserved
  */
 
+var request = require("request");
+
 var express = require('express');
 var router = express.Router();
 
 router.get('/eb-device/status', function(req, res, next) {
+
+    request({
+        url: 'http://myUrl.com/api/status',
+        json: true
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(body) // Print the json response
+        }
+    });
+
+
     res.json({ isRunning: true, errorText: 'testtesttest' });
 });
 
