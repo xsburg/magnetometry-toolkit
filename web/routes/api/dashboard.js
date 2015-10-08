5636cee73102b3048fb7c3675235c49680a22ce3
Stepan Burguchev <me@sburg.net>
2015-10-08 11:13:38 +0300

    some draft for command handling

--- web/routes/api/dashboard.js
+++ web/routes/api/dashboard.js
@@ -6,58 +6,43 @@
  */
 
 var request = require("request");
+var _ = require("underscore");
 var express = require('express');
 var router = express.Router();
 
 router.get('/eb-device/status', function(req, res) {
-
     request({
         url: 'http://simba.adm:8000/api/status',
         json: true
     }, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            console.log(body) // Print the json response
             res.json(body);
         }
-    });
-
-    /*request('http://www.google.com', function (error, response, body) {
-     if (!error && response.statusCode == 200) {
-     console.log(body) // Show the HTML for the Google homepage.
-     }
-     })*/
-
-return;
-    res.json({
-        isRunning: false,
-        updated: new Date(),
-        standBy: true,
-        samplingIntervalMs: 1000,
-        range: {
-            minField: 47500,
-            maxField: 55000
-        },
-        enq: 'MagState version 3.03',
-        about: 'MagState version 3.03\r\n\
-            Quantum Magnetometry Laboratory\r\n\
-        Urals State Technical University\r\n\
-        Copyright 2013.',
-        time: new Date(),
-        commandQueueSize: 2
     });
 });
 
 router.post('/eb-device/command', function (req, res) {
     var data = req.body;
-    switch (data.command) {
-    case 'standBy':
-        res.json({
-
-        });
-        break;
+    var availableCommands = [
+        'run',
+        'stop',
+        'update-status',
+        'set-device-time',
+        'set-device-range',
+        'set-device-stand-by'
+    ];
+    if (!_.contains(availableCommands, data.command)) {
+        res.status(500);
+        res.send('Invalid command');
+        return;
     }
-    res.json({
-
+    request({
+        url: 'http://simba.adm:8000/api/command',
+        json: true
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            res.json(body);
+        }
     });
 });
 
